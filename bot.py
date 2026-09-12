3bb05d3b5c3582b1ed3e33bb36a6327612e6749d
Bouba009 <algeria.ai007@gmail.com>
2026-09-12 15:43:50 +0100

    Update bot.py
    update

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print("BLS Appointment Monitor started")
+from playwright.sync_api import sync_playwright
+
+BLS_URL = "https://algeria.blsspainvisa.com/"
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    print("Opening BLS...")
+    page.goto(BLS_URL, wait_until="domcontentloaded", timeout=60000)
+
+    print("Page title:", page.title())
+    print("Current URL:", page.url)
+
+    browser.close()
+
+print("BLS test completed successfully.")
